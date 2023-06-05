c06c5afb1277a9a71b50cecffa213c970776ea37
modjordan <mod.jordan@icloud.com>
2023-06-05 16:17:17 -0700

    It will autoplay music on site.

--- java.js
+++ java.js
@@ -1,17 +1,17 @@
-window.onscroll = function () { myFunction() };
+// window.onscroll = function () { myFunction() };
 
-//get header
-var header = documnet.getElementById("myHeader");
+// //get header
+// var header = documnet.getElementById("myHeader");
 
-//get the offset position of the navbad
-var sticky = header.offsetTop;
+// //get the offset position of the navbad
+// var sticky = header.offsetTop;
 
-//add sticky class to header when you reach it's scroll posotion. remove "sticky" when you leave scroll position
+// //add sticky class to header when you reach it's scroll posotion. remove "sticky" when you leave scroll position
 
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky")
-  }
-}
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky")
+//   }
+// }
